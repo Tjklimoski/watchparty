fadeca0d4e971927f63fba86c97ad0ab6035a47f
TJ Klimoski <tjklimoski@gmail.com>
2023-07-27 14:34:02 -0400

    Style Navbar container

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className='sticky top-0 w-full max-w-screen-2xl px-6 py-2 md:px-12 md:py-4 flex items-center gap-4 md:gap-6 bg-base-100 bg-opacity-75 backdrop-blur-md'>
+      <div>
+        <h1 className='text-3xl'>Watch<span className='text-primary'>Party</span></h1>
+      </div>
+    </div>
   )
 }
