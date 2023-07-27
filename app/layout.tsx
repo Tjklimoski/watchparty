cdc1b3800702f2ad33c74cd4d0c0224e9e3ba668
TJ Klimoski <tjklimoski@gmail.com>
2023-07-27 14:17:14 -0400

    Create Navbar component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Navbar from '@/components/Navbar'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Rubik } from 'next/font/google'
@@ -16,7 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html data-theme="watchPartyDark" lang="en">
-      <body className={`${rubik.className}`}>{children}</body>
+      <body className={`${rubik.className}`}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 export default function Home() {
   return (
     <main>
-      <h1 className="text-3xl text-primary">WatchParty</h1>
+      <div className="flex gap-2 m-4 justify-center">
+        <label>Movie:</label>
+        <input type="search" className="input bg-neutral"/>
+        <button className="btn btn-primary">SEARCH</button>
+      </div>
     </main>
   )
 }
