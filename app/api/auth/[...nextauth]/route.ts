d8346e1a30fe277b8317af4ee45d66ff31c9529d
TJ Klimoski <tjklimoski@gmail.com>
2023-08-06 18:45:28 -0400

    Add Credential and Facebook provider options for nextauth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth";
 import type { AuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from 'next-auth/providers/google';
+import FacebookProvider from "next-auth/providers/facebook";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 
@@ -8,10 +10,30 @@ const prisma = new PrismaClient();
 
 export const config: AuthOptions = {
   adapter: PrismaAdapter(prisma),
-  providers: [GoogleProvider({
-    clientId: process.env.GOOGLE_CLIENT_ID ?? '',
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
-  })]
+  providers: [
+    CredentialsProvider({
+      name: 'your Email and Password',
+      type: 'credentials',
+      credentials: {
+        email: { label: 'Email', type: 'email' },
+        password: { label: 'Password', type: 'password' }
+      },
+      async authorize(credentials, req) {
+        // Logic to take the credentials that were submitted and return either:
+        // 1. a user object representing the user
+        // 2. fasle/null if credentials are invalid
+        return null;
+      }
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+    }),
+    FacebookProvider({
+      clientId: process.env.FB_APP_ID ?? '',
+      clientSecret: process.env.FB_APP_SECRET ?? '',
+    }),
+  ],
 }
 
 export default NextAuth(config)
