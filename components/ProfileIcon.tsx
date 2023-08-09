7cc3889f9dbffbff486f96f514ea2ff60ffaed06
TJ Klimoski <tjklimoski@gmail.com>
2023-08-09 11:57:58 -0400

    Configure profile icon to display default user icon if there's no user image

--- components/ProfileIcon.tsx
+++ components/ProfileIcon.tsx
@@ -1,15 +1,24 @@
+"use client";
+
 import Image from "next/image";
+import { FaUser } from "react-icons/fa6";
 
 export default function ProfileIcon() {
+  const user = false;
+
   return (
     <div className="avatar">
       <div className="rounded-full group-focus:ring group-hover:ring ring-primary ring-offset-base-100 ring-offset-2 hover:cursor-pointer transition">
-        <Image
-          width={40}
-          height={40}
-          src="/img/profile/avatar.jpg"
-          alt="profile image"
-        />
+        {user ? (
+          <Image
+            width={40}
+            height={40}
+            src="/img/profile/avatar.jpg"
+            alt="profile image"
+          />
+        ) : (
+          <FaUser size={30} />
+        )}
       </div>
     </div>
   );
