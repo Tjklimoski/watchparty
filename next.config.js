ec8cfdfaf1540043ef27277743709f91db4471f1
TJ Klimoski <tjklimoski@gmail.com>
2023-08-09 11:58:30 -0400

    Configure Next to allow images from OAuth provider domains

--- next.config.js
+++ next.config.js
@@ -2,11 +2,31 @@
 const nextConfig = {
   images: {
     remotePatterns: [
+      // To allow poster and backdrop images from tmdb api:
       {
         protocol: "https",
         hostname: "image.tmdb.org",
         port: "",
         pathname: "/t/p/**",
+      },
+      // To allow user profile images from various OAuth providers:
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
+        pathname: "/u/**",
+      },
+      {
+        protocol: "https",
+        hostname: "*.googleusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "platform-lookaside.fbsbx.com",
+        port: "",
+        pathname: "/platform/profilepic/**",
       },
     ],
   },
