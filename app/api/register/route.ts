6cc235be696ce1ba94023e10ddd6103a6158cc88
TJ Klimoski <tjklimoski@gmail.com>
2023-08-10 15:32:33 -0400

    Complete post route for registering a new user

---
+++ app/api/register/route.ts
@@ -0,0 +1,44 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/prisma/client";
+import bcrypt from 'bcrypt'
+import { capitalize } from "@/lib/stringModifications";
+
+interface RegisterData {
+  email: string;
+  password: string;
+}
+
+export async function POST(req: NextRequest) {
+  try {
+    const { email, password }: RegisterData = await req.json()
+
+    // Check to see if user already exists:
+    const user = await prisma.user.findUnique({
+      where: {
+        email
+      }
+    });
+
+    if (user) throw new Error('User already exists');
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    // Create a name for the user based of the user's email:
+    const name = capitalize(email.split('@')[0]);
+
+    const newUser = await prisma.user.create({
+      data: {
+        email,
+        name,
+        image: '',
+        password: hashedPassword,
+      }
+    })
+
+    return NextResponse.json(newUser);
+
+  } catch (err) {
+    console.error(err);
+    return new NextResponse(JSON.stringify(err), { status: 400 })
+  }
+}
