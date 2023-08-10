d228ecea5210715869c09fc1e81f109be3817e3b
TJ Klimoski <tjklimoski@gmail.com>
2023-08-09 20:27:28 -0400

    ensure for and id attributes in Input componenet are lowercase and kebab case

--- components/Input.tsx
+++ components/Input.tsx
@@ -6,15 +6,19 @@ function capitalize(word: string) {
   return word[0].toUpperCase() + word.slice(1);
 }
 
+function kebab(word: string) {
+  return word.split(" ").join("-");
+}
+
 export default function Input({ label, ...props }: InputProps) {
   return (
     <>
-      <label htmlFor={label} className="sr-only">
+      <label htmlFor={kebab(label.toLowerCase())} className="sr-only">
         {capitalize(label)}:
       </label>
       <input
         className="bg-neutral text-md sm:text-xl rounded-md px-4 sm:px-6 py-3 focus:outline outline-primary outline-offset-0 outline-2 w-full min-w-[150px]"
-        id={label}
+        id={kebab(label.toLowerCase())}
         placeholder={capitalize(label)}
         {...props}
       />
