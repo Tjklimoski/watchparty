1e46a1637a83bec4761da011109069fdbdc62222
TJ Klimoski <tjklimoski@gmail.com>
2023-08-09 21:23:33 -0400

    Pull out className prop in order to extend exisiting classes

--- components/Input.tsx
+++ components/Input.tsx
@@ -10,14 +10,16 @@ function kebab(word: string) {
   return word.split(" ").join("-");
 }
 
-export default function Input({ label, ...props }: InputProps) {
+export default function Input({ label, className, ...props }: InputProps) {
   return (
     <>
       <label htmlFor={kebab(label.toLowerCase())} className="sr-only">
         {capitalize(label)}:
       </label>
       <input
-        className="bg-neutral text-md sm:text-xl rounded-md px-4 sm:px-6 py-3 focus:outline outline-primary outline-offset-0 outline-2 w-full min-w-[150px]"
+        className={`bg-neutral text-md sm:text-xl rounded-md px-4 sm:px-6 py-3 focus:outline outline-primary outline-offset-0 outline-2 w-full min-w-[150px] ${
+          className ?? ""
+        }`}
         id={kebab(label.toLowerCase())}
         placeholder={capitalize(label)}
         {...props}
