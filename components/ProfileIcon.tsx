647bb635f9fd02fedfb8921e8958d58d30f4abf9
TJ Klimoski <tjklimoski@gmail.com>
2023-08-11 16:10:41 -0400

    implement useUser hook to populate user profile image src

--- components/ProfileIcon.tsx
+++ components/ProfileIcon.tsx
@@ -1,25 +1,31 @@
 "use client";
 
-import { User } from "@/types";
+import useUser from "@/hooks/useUser";
 import Image from "next/image";
 import { FaUser } from "react-icons/fa6";
 
-interface ProfileIconProps {
-  user: User;
-}
+const SIZE = 40;
 
-export default function ProfileIcon({ user }: ProfileIconProps) {
+export default function ProfileIcon() {
   // Since user is an async function, need to make sure the data
   // has been fetched before trying to access a field
-  const userImage = (user && user.image) ?? "";
+  const { user, isLoading } = useUser();
+  const userImage = user?.image ?? "";
 
   return (
     <div className="avatar">
       <div className="rounded-full group-focus:ring group-hover:ring ring-primary ring-offset-base-100 ring-offset-2 hover:cursor-pointer transition">
-        {userImage ? (
-          <Image width={40} height={40} src={userImage} alt="profile image" />
+        {isLoading ? (
+          <FaUser size={SIZE} />
+        ) : userImage ? (
+          <Image
+            width={SIZE}
+            height={SIZE}
+            src={userImage}
+            alt="profile image"
+          />
         ) : (
-          <FaUser size={30} />
+          <FaUser size={SIZE} />
         )}
       </div>
     </div>
