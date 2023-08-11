e74bf5cf83ab5b49afcd5b01ae1db0830d8f5349
TJ Klimoski <tjklimoski@gmail.com>
2023-08-11 11:55:12 -0400

    Setup API fetcher

---
+++ lib/APIFetcher.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: 'http://localhost:3000/api',
+})
+
+export default async function fetcher(url: string) {
+  return API.get(url).then(res => res.data);
+}
