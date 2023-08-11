4143ba92406132c22c66ca945486e1edc39fa8c7
TJ Klimoski <tjklimoski@gmail.com>
2023-08-11 16:34:01 -0400

    Add id and size props to ProfileIcon

--- components/ProfileIcon.tsx
+++ components/ProfileIcon.tsx
@@ -4,28 +4,31 @@ import useUser from "@/hooks/useUser";
 import Image from "next/image";
 import { FaUser } from "react-icons/fa6";
 
-const SIZE = 40;
+interface ProfileIconProps {
+  id?: string;
+  size?: number;
+}
 
-export default function ProfileIcon() {
+export default function ProfileIcon({ id, size = 35 }: ProfileIconProps) {
   // Since user is an async function, need to make sure the data
   // has been fetched before trying to access a field
-  const { user, isLoading } = useUser();
+  const { user, isLoading } = useUser(id);
   const userImage = user?.image ?? "";
 
   return (
     <div className="avatar">
       <div className="rounded-full group-focus:ring group-hover:ring ring-primary ring-offset-base-100 ring-offset-2 hover:cursor-pointer transition">
         {isLoading ? (
-          <FaUser size={SIZE} />
+          <FaUser size={size} />
         ) : userImage ? (
           <Image
-            width={SIZE}
-            height={SIZE}
+            width={size}
+            height={size}
             src={userImage}
             alt="profile image"
           />
         ) : (
-          <FaUser size={SIZE} />
+          <FaUser size={size} />
         )}
       </div>
     </div>
