3b6053258a2f243953e49fbf03a8e0b5ef7af8c7
TJ Klimoski <tjklimoski@gmail.com>
2023-08-18 14:43:01 -0400

    Set carousel columns width to min-content to prevent wide columns from forming when there are few children populating carousel

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -21,7 +21,7 @@ export default function Carousel({
       <div
         className={`grid ${
           tight ? "gap-2" : "gap-4"
-        } grid-flow-col overflow-x-scroll overscroll-x-contain snap-proximity snap-x snap-start pb-2 scrollbar-thin scrollbar-thumb-secondary active:scrollbar-thumb-secondary/80 scrollbar-track-neutral scrollbar-thumb-rounded-full scrollbar-track-rounded-full mb-4 md:mb-8`}
+        } grid-flow-col grid-cols-[repeat(25,min-content)] overflow-x-scroll overscroll-x-contain snap-proximity snap-x snap-start pb-2 scrollbar-thin scrollbar-thumb-secondary active:scrollbar-thumb-secondary/80 scrollbar-track-neutral scrollbar-thumb-rounded-full scrollbar-track-rounded-full mb-4 md:mb-8`}
       >
         {children}
       </div>
