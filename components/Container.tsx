91e55a0422a1d020fe1c2a299153eb78d218c631
TJ Klimoski <tjklimoski@gmail.com>
2023-08-19 13:32:01 -0400

    Remove commented code from Container component

--- components/Container.tsx
+++ components/Container.tsx
@@ -9,15 +9,3 @@ export default function Container({ children }: PageContainerProps) {
     </div>
   );
 }
-
-// export default function PageContainer({
-//   type: Tag = "main",
-//   styles = "",
-//   children,
-// }: PageContainerProps) {
-//   return (
-//     <Tag className={`px-6 py-2 md:px-12 md:py-4 ${styles}`}>
-//       <div className="w-full h-full max-w-[1440px] mx-auto">{children}</div>
-//     </Tag>
-//   );
-// }
