d159a67b3b81a5e23f7bce2204e9c5473874e43b
TJ Klimoski <tjklimoski@gmail.com>
2023-08-25 22:50:56 -0400

    Add glow effects to search bar

--- components/util/SearchBar.tsx
+++ components/util/SearchBar.tsx
@@ -5,11 +5,11 @@ export default function SearchBar() {
     <form className="flex flex-col min-[450px]:flex-row justify-center items-center gap-2 py-10">
       <Input
         label="Search Movies & TV"
-        className="max-w-lg drop-shadow-lg outline outline-1 outline-primary rounded-full"
+        className="max-w-lg focus:shadow-xl focus:shadow-primary/30  hover:shadow-xl hover:shadow-primary/30 transition duration-150 outline outline-1 outline-primary rounded-full"
       />
       <button
         type="submit"
-        className="btn bg-gradient-to-tr from-primary to-accent via-secondary via-50% text-base-100 drop-shadow-lg rounded-full w-full min-[450px]:w-min"
+        className="btn border-none bg-gradient-to-tr from-primary to-accent via-secondary via-50% text-base-100 rounded-full w-full min-[450px]:w-min hover:shadow-xl focus:shadow-xl hover:shadow-primary/30 focus:shadow-primary/30 focus:outline-primary transition duration-150"
       >
         Search
       </button>
