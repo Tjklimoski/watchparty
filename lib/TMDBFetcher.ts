b7bc6d9b317a50f598e20053e7031dbaf6498f5c
TJ Klimoski <tjklimoski@gmail.com>
2023-08-26 16:31:30 -0400

    Modify fetcher to correctly type and handle a /search/multi endpoint

--- lib/TMDBFetcher.ts
+++ lib/TMDBFetcher.ts
@@ -3,6 +3,10 @@
 import { Movie, TVShow } from "@/types";
 import axios from "axios";
 
+interface APIDataResults {
+  media_type?: 'tv' | 'movie' | 'person'
+}
+
 const TMDBApi = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   headers: {
@@ -20,7 +24,13 @@ export default async function fetcher(url: string) {
   const media_type: string = url.split('/')[1];
   return TMDBApi.get(url).then((res) => {
     if (Array.isArray(res.data?.results)) {
-      return res.data.results.map((result: Movie | TVShow) => ({ ...result, media_type }))
+      return res.data.results.map((result: APIDataResults) => {
+        const APIMediaType = result?.media_type;
+        if (!APIMediaType) return { ...result, media_type }
+        if (APIMediaType === 'person') return undefined;
+        return result;
+      }).filter((result: APIDataResults) => result);
+      // The .filter() above removes any undefined results in the array (a person that was returned in a search -- we only return movie or tv show objects)
     } else {
       return { ...res.data, media_type }
     }
