795e11980f32c8886892db03c8a8d4f67f5872de
TJ Klimoski <tjklimoski@gmail.com>
2023-08-26 15:01:40 -0400

    Handle onSubmit for searchBar component

--- components/util/SearchBar.tsx
+++ components/util/SearchBar.tsx
@@ -1,11 +1,33 @@
+"use client";
+
+import { FormEvent, useState } from "react";
 import Input from "../form/Input";
+import { useRouter } from "next/navigation";
 
 export default function SearchBar() {
+  // s is the value of the search the user is typing, passed in searchParam.
+  const [s, setS] = useState("");
+  const router = useRouter();
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    const path = new URL("/media/search?", window.location.origin);
+    const searchParams = new URLSearchParams({ s });
+    const url = `${path}${searchParams}`;
+    router.push(url);
+  }
+
   return (
-    <form className="flex flex-col min-[450px]:flex-row justify-center items-center gap-2 py-10">
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col min-[450px]:flex-row justify-center items-center gap-2 py-10"
+    >
       <Input
         label="Search Movies & TV"
         className="max-w-lg focus:shadow-xl focus:shadow-primary/30  hover:shadow-xl hover:shadow-primary/30 transition duration-150 outline outline-1 outline-primary rounded-full"
+        name="search"
+        value={s}
+        onChange={(e) => setS(e.target.value)}
       />
       <button
         type="submit"
