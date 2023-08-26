ddce8fd7bb403c4f940ea21bcdcaccaecde57b42
TJ Klimoski <tjklimoski@gmail.com>
2023-08-25 21:43:53 -0400

    Implement tailwindMerge on Input component

--- components/form/Input.tsx
+++ components/form/Input.tsx
@@ -1,4 +1,5 @@
 import { capitalize, kebab } from "@/lib/stringModifications";
+import { twMerge } from "tailwind-merge";
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label: string;
@@ -11,9 +12,11 @@ export default function Input({ label, className, ...props }: InputProps) {
         {capitalize(label)}:
       </label>
       <input
-        className={`bg-neutral text-md sm:text-xl rounded-md px-4 sm:px-6 py-3 focus:outline outline-primary outline-offset-0 outline-2 w-full min-w-[150px] [&:not(:focus):not(:placeholder-shown):invalid]:outline-error [&:not(:focus):not(:placeholder-shown):invalid]:outline ${
-          className ?? ""
-        }`}
+        className={twMerge(
+          `bg-neutral text-md sm:text-xl rounded-md px-4 sm:px-6 py-3 focus:outline outline-primary outline-offset-0 outline-2 w-full min-w-[150px] [&:not(:focus):not(:placeholder-shown):invalid]:outline-error [&:not(:focus):not(:placeholder-shown):invalid]:outline ${
+            className ?? ""
+          }`
+        )}
         id={kebab(label.toLowerCase())}
         placeholder={capitalize(label)}
         {...props}
