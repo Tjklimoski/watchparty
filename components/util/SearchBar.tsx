fbe6e14ac892c771f50e8fddb2477e6dd4100377
TJ Klimoski <tjklimoski@gmail.com>
2023-08-25 22:21:17 -0400

    Setup and style SearchBar component

--- components/util/SearchBar.tsx
+++ components/util/SearchBar.tsx
@@ -1,7 +1,18 @@
+import Input from "../form/Input";
+
 export default function SearchBar() {
   return (
-    <div className="h-10 w-96 m-4 text-center bg-zinc-700 rounded-md">
-      Search Bar
-    </div>
+    <form className="flex flex-col min-[450px]:flex-row justify-center items-center gap-2 py-10">
+      <Input
+        label="Search Movies & TV"
+        className="max-w-lg drop-shadow-lg outline outline-1 outline-primary rounded-full"
+      />
+      <button
+        type="submit"
+        className="btn bg-gradient-to-tr from-primary to-accent via-secondary via-50% text-base-100 drop-shadow-lg rounded-full w-full min-[450px]:w-min"
+      >
+        Search
+      </button>
+    </form>
   );
 }
