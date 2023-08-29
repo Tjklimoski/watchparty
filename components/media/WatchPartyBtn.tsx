5de8062537f46e7598009764604c367e5511aa75
TJ Klimoski <tjklimoski@gmail.com>
2023-08-29 17:42:10 -0400

    Setup search params and router for watchPartyBtn component

--- components/media/WatchPartyBtn.tsx
+++ components/media/WatchPartyBtn.tsx
@@ -1,20 +1,44 @@
+import { useEffect, useState } from "react";
 import { BiSolidParty } from "react-icons/bi";
+import { useRouter } from "next/navigation";
 
 interface WatchPartyBtnProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   mediaId: string;
+  media_type: "tv" | "movie";
+  season?: number;
+  episode?: number;
   sm?: boolean;
   tooltip?: boolean;
 }
 
 export default function WatchPartyBtn({
   mediaId: id,
+  media_type,
+  season,
+  episode,
   sm,
   tooltip,
   ...props
 }: WatchPartyBtnProps) {
+  const router = useRouter();
+  const [params, setParams] = useState({});
+  const searchParams = new URLSearchParams(params).toString();
+
+  useEffect(() => {
+    if (!media_type) return;
+    setParams(() => {
+      if (media_type === "movie") {
+        return { id, media_type };
+      } else {
+        return { id, media_type, season, episode };
+      }
+    });
+  }, [id, media_type, season, episode]);
+
   return (
     <button
+      onClick={() => router.push(`/watchparty/create?${searchParams}`)}
       className={`${
         sm ? "btn-sm" : "btn"
       } hover:btn-active focus-visible:btn-active btn-secondary btn-circle btn-outline border-2 ${
