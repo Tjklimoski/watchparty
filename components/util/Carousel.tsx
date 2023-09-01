0be5ff788ce29ef6c4ab31849f04152f1b9a1783
TJ Klimoski <tjklimoski@gmail.com>
2023-09-01 13:46:03 -0400

    Adjust how grid carousel lays out cards

--- components/util/Carousel.tsx
+++ components/util/Carousel.tsx
@@ -10,7 +10,7 @@ export default function Carousel({ children, tight = false }: CarouselProps) {
     <div
       className={`grid ${
         tight ? "gap-2" : "gap-4"
-      } grid-flow-col grid-cols-[repeat(auto,min-content)] overflow-x-scroll overscroll-x-contain snap-proximity snap-x snap-start pb-2 scrollbar-thin scrollbar-thumb-secondary active:scrollbar-thumb-secondary/80 scrollbar-track-neutral scrollbar-thumb-rounded-full scrollbar-track-rounded-full mb-2 sm:mb-4 @container`}
+      } grid-flow-col grid-cols-[repeat(10,min-content)] overflow-x-scroll overscroll-x-contain snap-proximity snap-x snap-start pb-2 scrollbar-thin scrollbar-thumb-secondary active:scrollbar-thumb-secondary/80 scrollbar-track-neutral scrollbar-thumb-rounded-full scrollbar-track-rounded-full mb-2 sm:mb-4 @container`}
     >
       {children}
     </div>
