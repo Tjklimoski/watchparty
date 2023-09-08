5845dfa0c35ab2fef129fd27fd6f88966a6a7f1e
TJ Klimoski <tjklimoski@gmail.com>
2023-09-07 20:20:43 -0400

    Send watchPartyData from API to database

---
+++ app/api/watchparties/route.ts
@@ -0,0 +1,22 @@
+import prisma from "@/prisma/client";
+import { NextRequest, NextResponse as res } from "next/server";
+
+export async function POST(req: NextRequest) {
+  console.log('/watchparties POST req called')
+  const data = await req.json();
+  console.log('data: ', data);
+
+  try {
+    const watchParty = await prisma.watchParty.create({
+      data
+    });
+
+    if (!watchParty) return new res('Invalid data', { status: 400 })
+
+    return res.json(watchParty);
+
+  } catch (err) {
+    console.log('ERROR!: ', err)
+    return new res('Invalid data', { status: 400 });
+  }
+}
