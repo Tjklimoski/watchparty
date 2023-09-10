8366e950f12701c5e30b9ce2ba2696c67d75b938
TJ Klimoski <tjklimoski@gmail.com>
2023-09-10 18:29:36 -0400

    Set EpisodeCards to always snap to center when horizontally scrolled in carousel

--- components/util/Carousel.tsx
+++ components/util/Carousel.tsx
@@ -5,12 +5,12 @@ interface CarouselProps {
   tight?: boolean;
 }
 
-export default function Carousel({ children, tight = false }: CarouselProps) {
+export default function Carousel({ children, tight }: CarouselProps) {
   return (
     <div
       className={`grid ${
         tight ? "gap-2" : "gap-4"
-      } grid-flow-col grid-cols-[repeat(10,min-content)] overflow-x-scroll overscroll-x-contain snap-proximity snap-x snap-start pb-2 scrollbar-thin scrollbar-thumb-secondary active:scrollbar-thumb-secondary/80 scrollbar-track-neutral scrollbar-thumb-rounded-full scrollbar-track-rounded-full mb-2 sm:mb-4 @container`}
+      } grid-flow-col grid-cols-[repeat(10,min-content)] overflow-x-scroll overscroll-x-contain snap-proximity snap-x pb-2 scrollbar-thin scrollbar-thumb-secondary active:scrollbar-thumb-secondary/80 scrollbar-track-neutral scrollbar-thumb-rounded-full scrollbar-track-rounded-full mb-2 sm:mb-4 @container`}
     >
       {children}
     </div>
