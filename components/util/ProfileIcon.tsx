55a449594ee8bc6e8c279e3761f32eab887861b4
TJ Klimoski <tjklimoski@gmail.com>
2023-09-12 16:03:08 -0400

    Apply outline to profileIcon if group prop is applied

--- components/util/ProfileIcon.tsx
+++ components/util/ProfileIcon.tsx
@@ -7,25 +7,42 @@ import { FaUser } from "react-icons/fa6";
 interface ProfileIconProps {
   id?: string;
   size?: number;
+  group?: boolean;
 }
 
-export default function ProfileIcon({ id, size = 38 }: ProfileIconProps) {
+export default function ProfileIcon({
+  id,
+  size = 38,
+  group,
+}: ProfileIconProps) {
   // Since user is an async function, need to make sure the data
   // has been fetched before trying to access a field
   const { user, isLoading } = useUser(id);
   const userImage = user?.image ?? "";
 
   return isLoading ? (
-    <FaUser size={size} className="rounded-full" />
+    <FaUser
+      size={size}
+      className={`rounded-full bg-neutral ${
+        group && "outline"
+      } outline-2 outline-primary group-focus:outline-primary-focus group-hover:outline-accent-focus p-1`}
+    />
   ) : userImage ? (
     <Image
       width={size}
       height={size}
       src={userImage}
       alt="profile image"
-      className="rounded-full"
+      className={`rounded-full ${
+        group && "outline"
+      } outline-2 outline-accent group-focus:outline-primary-focus group-hover:outline-accent-focus`}
     />
   ) : (
-    <FaUser size={size} className="rounded-full" />
+    <FaUser
+      size={size}
+      className={`rounded-full bg-neutral ${
+        group && "outline"
+      } outline-2 outline-accent group-focus:outline-primary-focus group-hover:outline-accent-focus p-1`}
+    />
   );
 }
