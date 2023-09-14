2158deaeaabc16b1dfa0d4d7f70cce60807bf2b3
TJ Klimoski <tjklimoski@gmail.com>
2023-09-14 17:54:16 -0400

    Create PUT route to handle updating an edited watchParty event

--- app/api/watchparties/[id]/route.ts
+++ app/api/watchparties/[id]/route.ts
@@ -1,7 +1,12 @@
 import prisma from "@/prisma/client";
+import { watch } from "fs";
 import { NextRequest, NextResponse as res } from "next/server";
 
-export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
+interface Params {
+  params: { id: string }
+}
+
+export async function GET(req: NextRequest, { params }: Params) {
   const { id } = params;
 
   try {
@@ -14,6 +19,28 @@ export async function GET(req: NextRequest, { params }: { params: { id: string }
     if (!watchParty) throw new Error("Invalid ID")
 
     return res.json(watchParty);
+  } catch (err: Error | any) {
+    return new res(err?.message ?? "Failed", { status: 400 })
+  }
+}
+
+// create PUT request for when the host user edits/updates the watchparty
+export async function PUT(req: NextRequest, { params }: Params) {
+  const { id } = params;
+  try {
+    const data = await req.json();
+
+    // Pass data to update() - any field not passed (left as undefiend) will not overwrite the current data in the document.
+    const updatedWatchParty = await prisma.watchParty.update({
+      where: {
+        id
+      },
+      data
+    });
+
+    if (!updatedWatchParty) throw new Error('Invalid data sent, unable to update')
+
+    return res.json(updatedWatchParty);
   } catch (err: Error | any) {
     return new res(err?.message ?? "Failed", { status: 400 })
   }
