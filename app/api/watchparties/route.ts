e0e15129c5a68210b5995dc22795bd01628833dd
TJ Klimoski <tjklimoski@gmail.com>
2023-09-15 14:06:17 -0400

    Make request to watchparty database searching based on user's coordinates and radius

--- app/api/watchparties/route.ts
+++ app/api/watchparties/route.ts
@@ -2,11 +2,39 @@ import prisma from "@/prisma/client";
 import { NextRequest, NextResponse as res } from "next/server";
 
 interface Body {
-  radius: number
+  radius: number;
+  coordinates: [number, number];
 }
 
 export async function GET(req: NextRequest) {
-  const { radius }: Body = await req.json();
+  try {
+    const { radius, coordinates }: Body = await req.json();
+
+    // convert radius from miles to meters (for mongodb) - 1 mile = 1609.34 meters
+    const radiusMeters = radius * 1609.34
+
+    const result = await prisma.watchParty.findRaw({
+      filter: {
+        geo: {
+          $near: {
+            $geometry: {
+              type: "point",
+              coordinates
+            },
+            $maxDistance: radiusMeters,
+          }
+        }
+      }
+    })
+
+    if (!result) throw new Error('Invalid request');
+
+    // res can be an empty array - meaning nothing found in radius
+    return res.json(result)
+  } catch (err: Error | any) {
+    console.error(err?.message ?? err)
+    return new res(err?.message ?? "request failed", { status: 400 })
+  }
 }
 
 export async function POST(req: NextRequest) {
