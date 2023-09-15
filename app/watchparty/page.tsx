8b3b2947865e96a606c1d81235e4b7aecf24ec2f
TJ Klimoski <tjklimoski@gmail.com>
2023-09-15 14:09:12 -0400

    Create WatchParty page

---
+++ app/watchparty/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import useUser from "@/hooks/useUser";
+import { API } from "@/lib/APIFetcher";
+import React from "react";
+
+export default function WatchPartyPage() {
+  return <div>WatchPartyPage</div>;
+}
