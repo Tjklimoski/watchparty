10ae2ae7edc8760ce5c4d269e75d2e42813b602a
TJ Klimoski <tjklimoski@gmail.com>
2023-09-18 15:04:22 -0400

    Modify ProfileIcon to fetch user data with SWR to take advantage of chaching

--- components/util/ProfileIcon.tsx
+++ components/util/ProfileIcon.tsx
@@ -1,8 +1,10 @@
 "use client";
 
-import useUser from "@/hooks/useUser";
+import APIFetcher from "@/lib/APIFetcher";
+import { LimitedUser } from "@/types";
 import Image from "next/image";
 import { FaUser } from "react-icons/fa6";
+import useSWR from "swr";
 
 interface ProfileIconProps {
   id?: string;
@@ -15,16 +17,17 @@ export default function ProfileIcon({
   size = 38,
   group,
 }: ProfileIconProps) {
-  // Since user is an async function, need to make sure the data
-  // has been fetched before trying to access a field
-  const { user, isLoading } = useUser(id);
-  const userImage = user?.image ?? "";
+  const { data: user, isLoading } = useSWR<LimitedUser>(
+    id && `/users/${id}`,
+    APIFetcher
+  );
+  const userImage = user?.image;
 
   return isLoading ? (
     <FaUser
       size={size}
-      className={`rounded-full ${group ? "bg-neutral" : "bg-neutral/60"} ${
-        group && "outline"
+      className={`rounded-full ${
+        group ? "bg-neutral outline" : "bg-neutral/60"
       } outline-2 outline-primary group-focus:outline-primary-focus group-hover:outline-accent-focus p-1 text-base-content`}
     />
   ) : userImage ? (
@@ -40,8 +43,8 @@ export default function ProfileIcon({
   ) : (
     <FaUser
       size={size}
-      className={`rounded-full ${group ? "bg-neutral" : "bg-neutral/60"} ${
-        group && "outline"
+      className={`rounded-full ${
+        group ? "bg-neutral outline" : "bg-neutral/60"
       } outline-2 outline-accent group-focus:outline-primary-focus group-hover:outline-accent-focus p-1 text-base-content`}
     />
   );
