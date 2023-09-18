0ca68dfa4551c559925fead426180ce9c9905304
TJ Klimoski <tjklimoski@gmail.com>
2023-09-18 18:28:59 -0400

    Create PopupHeader component

---
+++ components/util/PopupHeader.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { IoClose } from "react-icons/io5";
+
+interface PopupHeaderProps {
+  title: string;
+  handleClose: () => void;
+}
+
+export default function PopupHeader({ title, handleClose }: PopupHeaderProps) {
+  return (
+    <header className="flex flex-col-reverse xs:flex-row items-start mb-2 sm:mb-4">
+      <h3 className="uppercase text-2xl sm:text-3xl font-semibold">{title}</h3>
+      <div className="grow flex justify-end items-start self-stretch">
+        <button onClick={handleClose}>
+          <IoClose size={30} />
+        </button>
+      </div>
+    </header>
+  );
+}
