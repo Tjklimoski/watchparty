c45e38ef498653f8e52b82d5cdfb6172414de3a8
TJ Klimoski <tjklimoski@gmail.com>
2023-09-19 12:14:42 -0400

    Create EditBtn component for WatchParty events

---
+++ components/watchparty/EditBtn.tsx
@@ -0,0 +1,21 @@
+import { useRouter } from "next/navigation";
+import { MdEdit } from "react-icons/md";
+
+interface EditBtnProps {
+  watchPartyId: string;
+}
+
+export default function EditBtn({ watchPartyId }: EditBtnProps) {
+  const router = useRouter();
+
+  return (
+    <button
+      className="btn btn-circle btn-primary btn-outline border-2 grid place-items-center tooltip tooltip-info normal-case"
+      data-tip="Edit"
+      aria-label="Edit your WatchParty"
+      onClick={() => router.push(`/watchparty/${watchPartyId}/edit`)}
+    >
+      <MdEdit size={30} />
+    </button>
+  );
+}
