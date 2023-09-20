63c2344faf79e1e211059d22f3678eba70b35519
TJ Klimoski <tjklimoski@gmail.com>
2023-09-19 22:18:51 -0400

    Add confetti explosion animation when user clicks to attend a watchparty

--- components/watchparty/AttendBtn.tsx
+++ components/watchparty/AttendBtn.tsx
@@ -6,6 +6,7 @@ import useSWR from "swr";
 import { useEffect, useState } from "react";
 import apiFetcher, { API } from "@/lib/APIFetcher";
 import { BsPersonFillAdd, BsPersonFillDash } from "react-icons/bs";
+import Confetti from "react-confetti-explosion";
 
 interface MyListBtnProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   watchPartyId: string;
@@ -37,6 +38,7 @@ export default function AttendBtn({
   // Fetch current user
   const { data: user, mutate: userMutate } = useSWR<User>("/user", apiFetcher);
   const [attending, setAttending] = useState(false);
+  const [animateConfetti, setAnimateConfetti] = useState(false);
 
   // Fetch watchParty Data
   const { data: watchParty, error: watchPartyError } = useSWR<WatchParty>(
@@ -56,7 +58,11 @@ export default function AttendBtn({
   async function handleClick() {
     try {
       if (!user) throw new Error("No current user");
-      setAttending((current) => !current);
+      setAttending((current) => {
+        const isAttending = !current;
+        setAnimateConfetti(isAttending);
+        return isAttending;
+      });
 
       const updatedWatchParty = attending
         ? await deleteUserFromList(id, user.id)
@@ -72,22 +78,35 @@ export default function AttendBtn({
   }
 
   return (
-    <button
-      className={`${sm ? "btn-sm" : "btn"} btn-primary ${
-        !attending && "btn-outline"
-      } hover:btn-active focus-visible:btn-active border-2 btn-circle grid place-items-center ${
-        tooltip && "tooltip"
-      } tooltip-primary normal-case transition duration-300`}
-      data-tip={attending ? "Quit Attending" : "Attend WatchParty"}
-      aria-label={attending ? "Quit Attending" : "Attend WatchParty"}
-      {...props}
-      onClick={handleClick}
-    >
-      {attending ? (
-        <BsPersonFillDash size={sm ? 20 : 30} />
-      ) : (
-        <BsPersonFillAdd size={sm ? 20 : 30} />
-      )}
-    </button>
+    <>
+      <button
+        className={`relative ${sm ? "btn-sm" : "btn"} btn-primary ${
+          !attending && "btn-outline"
+        } hover:btn-active focus-visible:btn-active border-2 btn-circle grid place-items-center ${
+          tooltip && "tooltip"
+        } tooltip-primary normal-case transition duration-300`}
+        data-tip={attending ? "Quit Attending" : "Attend WatchParty"}
+        aria-label={attending ? "Quit Attending" : "Attend WatchParty"}
+        {...props}
+        onClick={handleClick}
+      >
+        {attending ? (
+          <BsPersonFillDash size={sm ? 20 : 30} />
+        ) : (
+          <BsPersonFillAdd size={sm ? 20 : 30} />
+        )}
+        {animateConfetti && (
+          <Confetti
+            className="absolute"
+            particleCount={50}
+            particleSize={10}
+            width={450}
+            force={0.43}
+            colors={["#3abff8", "#828df8", "#f471b5", "#2bd4bd"]}
+            onComplete={() => setAnimateConfetti(false)}
+          />
+        )}
+      </button>
+    </>
   );
 }
