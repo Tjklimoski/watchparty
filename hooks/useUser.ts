137af65eea115d74457b2b9c3e37c0af67682f35
TJ Klimoski <tjklimoski@gmail.com>
2023-09-20 17:27:18 -0400

    Modify useUser hook to take no arguments and only return the current user

--- hooks/useUser.ts
+++ hooks/useUser.ts
@@ -2,11 +2,8 @@ import useSWR from 'swr';
 import APIFetcher from '@/lib/APIFetcher';
 import { User } from '@/types';
 
-export default function useUser(id?: string) {
-  let route = '/user'
-  if (id) route = `/users/${id}`
-
-  const { data: user, isLoading, error, mutate } = useSWR<User>(route, APIFetcher)
+export default function useUser() {
+  const { data: user, isLoading, error, mutate } = useSWR<User>('/user', APIFetcher)
 
   return { user, isLoading, error, mutate }
 }
