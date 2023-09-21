d4ca99b7451bd36f6c84248f6c90988cc5ff1e98
TJ Klimoski <tjklimoski@gmail.com>
2023-09-20 22:02:21 -0400

    Create Watchparty edit page and pass watchparty values as input value defaults

---
+++ app/watchparty/[partyid]/edit/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import Container from "@/components/util/Container";
+import WatchPartyForm from "@/components/form/WatchPartyForm";
+import useSWR from "swr";
+import { WatchParty, WatchPartyInputs } from "@/types";
+import APIFetcher from "@/lib/APIFetcher";
+import { formatFullDate } from "@/lib/format";
+
+export default function CreateWatchPartyPage({
+  params,
+}: {
+  params: { partyid: string };
+}) {
+  const { partyid: id } = params;
+  const { data: watchParty, error } = useSWR<WatchParty>(
+    id && `/watchparties/${id}`,
+    APIFetcher
+  );
+  if (error) throw new Error("Invalid WatchParty Id");
+  if (!watchParty) return null;
+
+  // modify watchParty data into form Input values
+  const updateInputs: WatchPartyInputs = {
+    ...watchParty,
+    date: formatFullDate(new Date(watchParty.date)), // returns "YYYY-MM-DD"
+    time: new Date(watchParty.date).toLocaleTimeString(),
+    zip: watchParty.zip,
+  };
+
+  return (
+    <main className="min-h-screen">
+      <Container>
+        <WatchPartyForm {...watchParty} updateInputs={updateInputs} update />
+      </Container>
+    </main>
+  );
+}
