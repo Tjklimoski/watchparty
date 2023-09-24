23658aee687af4b5120ca1f519edd2c52d160d1f
TJ Klimoski <tjklimoski@gmail.com>
2023-09-24 14:32:19 -0400

    Change disabled opacity strength to 60

--- components/form/Input.tsx
+++ components/form/Input.tsx
@@ -13,7 +13,7 @@ export default function Input({ label, className, ...props }: InputProps) {
       </label>
       <input
         className={twMerge(
-          `bg-neutral text-md sm:text-xl rounded-md px-4 sm:px-6 py-2 focus:outline outline-primary outline-offset-0 outline-2 w-full min-w-[150px] [&:not(:focus):not(:placeholder-shown):invalid]:outline-error [&:not(:focus):not(:placeholder-shown):invalid]:outline disabled:opacity-80 disabled:cursor-not-allowed ${
+          `bg-neutral text-md sm:text-xl rounded-md px-4 sm:px-6 py-2 focus:outline outline-primary outline-offset-0 outline-2 w-full min-w-[150px] [&:not(:focus):not(:placeholder-shown):invalid]:outline-error [&:not(:focus):not(:placeholder-shown):invalid]:outline disabled:opacity-60 disabled:cursor-not-allowed ${
             className ?? ""
           }`
         )}
