90ed7214d8f2c7f5637cb5ac687bdc418f812912
TJ Klimoski <tjklimoski@gmail.com>
2023-09-26 13:38:48 -0400

    Create WatchPartyCard component

---
+++ components/watchparty/WatchPartyCard.tsx
@@ -0,0 +1,10 @@
+import { WatchParty } from "@/types";
+import React from "react";
+
+interface WatchPartyCardProps {
+  watchParty: WatchParty;
+}
+
+export default function WatchPartyCard({ watchParty }: WatchPartyCardProps) {
+  return <div>WatchPartyCard</div>;
+}
