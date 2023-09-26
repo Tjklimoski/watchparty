2b1fe673a4221d21774ffa9fe89c2c9fe5030a0a
TJ Klimoski <tjklimoski@gmail.com>
2023-09-26 13:41:56 -0400

    Fetch media data for watchParty and extract img information

--- components/watchparty/WatchPartyCard.tsx
+++ components/watchparty/WatchPartyCard.tsx
@@ -1,10 +1,20 @@
+import TMDBfetcher from "@/lib/TMDBFetcher";
 import { WatchParty } from "@/types";
 import React from "react";
+import useSWR from "swr";
 
 interface WatchPartyCardProps {
   watchParty: WatchParty;
 }
 
 export default function WatchPartyCard({ watchParty }: WatchPartyCardProps) {
+  const { data: media, error: mediaError } = useSWR(
+    `/${watchParty.mediaType}/${watchParty.mediaId}`,
+    TMDBfetcher
+  );
+  const baseImgPath = "https://image.tmdb.org/t/p/";
+  const imgSize = "w780";
+  const title = media?.media_type === "movie" ? media?.title : media?.name;
+
   return <div>WatchPartyCard</div>;
 }
