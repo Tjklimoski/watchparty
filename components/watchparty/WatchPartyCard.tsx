9c895501d2463b8bcdfaf4f2247b17bc1ca80bf7
TJ Klimoski <tjklimoski@gmail.com>
2023-09-26 14:48:57 -0400

    Add Distance componenet below the WatchPartyCard

--- components/watchparty/WatchPartyCard.tsx
+++ components/watchparty/WatchPartyCard.tsx
@@ -5,6 +5,7 @@ import useSWR from "swr";
 import Skeleton from "../util/Skeleton";
 import Link from "next/link";
 import Image from "next/image";
+import Distance from "./Distance";
 
 interface WatchPartyCardProps {
   watchParty: WatchParty | undefined;
@@ -21,29 +22,34 @@ export default function WatchPartyCard({ watchParty }: WatchPartyCardProps) {
   return !media || !watchParty ? (
     <Skeleton className="h-full w-48 @lg:w-52 @3xl:w-64 @5xl:w-72 aspect-video rounded-sm" />
   ) : (
-    <div className="relative w-48 @lg:w-52 @3xl:w-64 @5xl:w-72 aspect-video rounded-sm drop-shadow-lg snap-center @lg:snap-start group overflow-hidden">
-      <Image
-        className="object-cover brightness-90 group-hover:brightness-100 group-focus-within:brightness-100 rounded-sm transition duration-150"
-        src={
-          media.backdrop_path
-            ? baseImgPath + imgSize + media.backdrop_path
-            : "/img/placeholder-md.jpg"
-        }
-        alt={`${watchParty.title} Billboard`}
-        fill
-      />
+    <div>
+      <div className="relative w-48 @lg:w-52 @3xl:w-64 @5xl:w-72 mb-1 aspect-video rounded-sm drop-shadow-lg snap-center @lg:snap-start group overflow-hidden">
+        <Image
+          className="object-cover brightness-90 group-hover:brightness-100 group-focus-within:brightness-100 rounded-sm transition duration-150"
+          src={
+            media.backdrop_path
+              ? baseImgPath + imgSize + media.backdrop_path
+              : "/img/placeholder-md.jpg"
+          }
+          alt={`${watchParty.title} Billboard`}
+          fill
+        />
 
-      {/* Title and Interaction Buttons container */}
-      <div className="absolute left-0 bottom-0 right-0 h-1/2 bg-gradient-to-t from-black via-black via-45% to-transparent p-1 @lg:p-2 flex justify-between items-end select-none">
-        <h3 className="font-semibold text-md @lg:text-lg @3xl:text-xl break-balance webkit-truncate">
-          {watchParty.title}
-        </h3>
+        {/* Title and Interaction Buttons container */}
+        <div className="absolute left-0 bottom-0 right-0 h-1/2 bg-gradient-to-t from-black via-black via-45% to-transparent p-1 @lg:p-2 flex justify-between items-end select-none">
+          <h3 className="font-semibold text-md @lg:text-lg @3xl:text-xl break-balance webkit-truncate">
+            {watchParty.title}
+          </h3>
+        </div>
+
+        <Link
+          href={`/watchparty/${watchParty.id}`}
+          className="absolute block inset-0 focus:border-2 border-primary"
+        />
       </div>
 
-      <Link
-        href={`/watchparty/${watchParty.id}`}
-        className="absolute block inset-0 focus:border-2 border-primary"
-      />
+      {/* mongodb, when using aggreagate query with $geoSphere, adds a dist.calculated field to the data, which is the mile distance between the two points */}
+      <Distance watchPartyCoords={watchParty.geo.coordinates} />
     </div>
   );
 }
