234cd8e065a2b120d41e53f70b4b84e7d416e7cc
TJ Klimoski <tjklimoski@gmail.com>
2023-09-28 18:28:15 -0400

    Modify APIFetcher to take either a string as the URL or an object containing a url and params

--- lib/APIFetcher.ts
+++ lib/APIFetcher.ts
@@ -1,9 +1,26 @@
 import axios from "axios";
 
+interface APIFetcherOptions {
+  url: string,
+  params: {
+    radius: number,
+    coordinates: [number, number],
+    query?: string,
+    page?: number,
+  }
+}
+
 export const API = axios.create({
   baseURL: 'http://localhost:3000/api',
 })
 
-export default async function fetcher(url: string) {
-  return API.get(url).then(res => res.data).catch(err => { throw new Error(err?.response?.data?.message ?? 'Server error') });
+export default async function fetcher(options: string | APIFetcherOptions) {
+  if (typeof options === "string") {
+    const url = options
+    return API.get(url).then(res => res.data).catch(err => { throw new Error(err?.response?.data?.message ?? 'Server error') });
+  } else {
+    const url = options.url;
+    const params = options.params;
+    return API.get(url, { params }).then(res => res.data).catch(err => { throw new Error(err?.response?.data?.message ?? 'Server error') });
+  }
 }
