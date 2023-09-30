066eb753fea851e88b3b6b01fd21e083cdc071c5
TJ Klimoski <tjklimoski@gmail.com>
2023-09-30 17:09:44 -0400

    Add loading state for PageCount component

--- components/util/PageCount.tsx
+++ components/util/PageCount.tsx
@@ -1,5 +1,6 @@
 import { useSearchParams } from "next/navigation";
 import React from "react";
+import Skeleton from "./Skeleton";
 
 interface PageCountProps {
   totalPages: number | undefined;
@@ -7,9 +8,11 @@ interface PageCountProps {
 
 export default function PageCount({ totalPages }: PageCountProps) {
   const params = useSearchParams();
-  const page = params.get("page") ?? "NA";
+  const page = params.get("page") ?? "1";
 
-  return (
+  return !totalPages ? (
+    <Skeleton className="w-full h-7 max-w-[12ch]" />
+  ) : (
     <span className="ml-2">
       page <span className="font-semibold">{page}</span> of{" "}
       <span className="font-semibold">{totalPages}</span>
