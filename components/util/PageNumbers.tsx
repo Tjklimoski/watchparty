4467ddbcc31c918c2660f9159869102627196ad8
TJ Klimoski <tjklimoski@gmail.com>
2023-09-30 16:16:09 -0400

    Create URL, set page search param on click and push user to new route

--- components/util/PageNumbers.tsx
+++ components/util/PageNumbers.tsx
@@ -1,9 +1,53 @@
-import React from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import Skeleton from "./Skeleton";
 
 interface PageNumbersProps {
-  totalPages: number;
+  totalPages: number | undefined;
 }
 
 export default function PageNumbers({ totalPages }: PageNumbersProps) {
-  return <div>PageNumbers</div>;
+  const router = useRouter();
+  // get and check current url for page searchParam. if no page param is present, default value to "1"
+  const params = useSearchParams();
+  const page = params.get("page") ?? "1";
+  // Create a url based off the current URL
+  const url = new URL(window.location.href);
+
+  return (
+    <div className="flex justify-center mt-8">
+      <ul className="flex flex-wrap justify-center gap-4 max-w-lg">
+        {!totalPages
+          ? Array(5)
+              .fill(null)
+              .map((item, i) => (
+                <li key={`loading${i}`}>
+                  <Skeleton className="w-5 h-6 rounded-sm" />
+                </li>
+              ))
+          : Array(totalPages)
+              .fill(null)
+              .map((item, i) => {
+                const pageNumber = i + 1;
+                return (
+                  <li key={pageNumber}>
+                    <button
+                      onClick={() => {
+                        // Set page serach param on url and push user to new route.
+                        url.searchParams.set("page", pageNumber.toString());
+                        router.push(url.href);
+                      }}
+                      className={`cursor-pointer text-xl hover:text-primary focus:text-primary outline-none ${
+                        pageNumber.toString() === page
+                          ? "text-accent underline-offset-4 underline"
+                          : ""
+                      }`}
+                    >
+                      {pageNumber}
+                    </button>
+                  </li>
+                );
+              })}
+      </ul>
+    </div>
+  );
 }
