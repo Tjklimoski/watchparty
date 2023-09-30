0b52747d0138041336339c52b3ace27f94db261a
TJ Klimoski <tjklimoski@gmail.com>
2023-09-30 15:48:36 -0400

    Setup PageNumbers component, which will render clickable pages for search results

---
+++ components/util/PageNumbers.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface PageNumbersProps {
+  totalPages: number;
+}
+
+export default function PageNumbers({ totalPages }: PageNumbersProps) {
+  return <div>PageNumbers</div>;
+}
