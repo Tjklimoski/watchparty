a086cf4760c5af01613a6f98c46dbb8c3cf9d9fd
TJ Klimoski <tjklimoski@gmail.com>
2023-09-30 23:05:01 -0400

    Set location state with window.location data, and return values

--- hooks/useWindowLocation.tsx
+++ hooks/useWindowLocation.tsx
@@ -1,5 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+interface WindowLocation {
+  href: string | undefined;
+  origin: string | undefined;
+  pathname: string | undefined;
+  port: string | undefined;
+  search: string | undefined;
+  toString: (() => string) | undefined;
+}
 
 export default function useWindowLocation() {
-  const [location, setLocation] = useState({});
+  const [location, setLocation] = useState<WindowLocation>({
+    href: undefined,
+    origin: undefined,
+    pathname: undefined,
+    port: undefined,
+    search: undefined,
+    toString: undefined,
+  });
+
+  // use effect will run once on mount, so window will be defined
+  useEffect(() => {
+    const { href, origin, pathname, port, search, toString } = window.location;
+    setLocation({ href, origin, pathname, port, search, toString });
+  }, []);
+
+  return location;
 }
