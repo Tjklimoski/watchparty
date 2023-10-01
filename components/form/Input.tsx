586d756f1a85386fe86fce45101ed246d0eff054
TJ Klimoski <tjklimoski@gmail.com>
2023-09-30 23:44:10 -0400

    Allow refs to be forwarded to Input component

--- components/form/Input.tsx
+++ components/form/Input.tsx
@@ -1,17 +1,22 @@
 import { capitalize, kebab } from "@/lib/stringModifications";
+import { forwardRef } from "react";
 import { twMerge } from "tailwind-merge";
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label: string;
 }
 
-export default function Input({ label, className, ...props }: InputProps) {
+const Input = forwardRef<HTMLInputElement, InputProps>(function Input(
+  { label, className, ...props },
+  ref
+) {
   return (
     <>
       <label htmlFor={kebab(label.toLowerCase())} className="sr-only">
         {capitalize(label)}:
       </label>
       <input
+        ref={ref}
         className={twMerge(
           `bg-neutral text-md sm:text-xl rounded-md px-4 sm:px-6 py-2 focus:outline outline-primary outline-offset-0 outline-2 w-full min-w-[150px] [&:not(:focus):not(:placeholder-shown):invalid]:outline-error [&:not(:focus):not(:placeholder-shown):invalid]:outline disabled:opacity-60 disabled:cursor-not-allowed ${
             className ?? ""
@@ -23,4 +28,6 @@ export default function Input({ label, className, ...props }: InputProps) {
       />
     </>
   );
-}
+});
+
+export default Input;
