4b21835910235625b5d200202ad70151e7c06e27
TJ Klimoski <tjklimoski@gmail.com>
2023-10-01 22:45:30 -0400

    Improve code readability with prettier

--- lib/APIFetcher.ts
+++ lib/APIFetcher.ts
@@ -1,27 +1,35 @@
 import axios from "axios";
 
 interface APIFetcherOptions {
-  url: string,
+  url: string;
   params: {
-    radius: number,
-    coordinates: [number, number],
-    query?: string,
-    page?: number,
-  }
+    radius: number;
+    coordinates: [number, number];
+    query?: string;
+    page?: number;
+  };
 }
 
 export const API = axios.create({
-  baseURL: 'http://localhost:3000/api',
-})
+  baseURL: "http://localhost:3000/api",
+});
 
 export default async function fetcher(options: string | APIFetcherOptions) {
   if (!options) return null;
   if (typeof options === "string") {
-    const url = options
-    return API.get(url).then(res => res.data).catch(err => { throw new Error(err?.response?.data?.message ?? 'Server error') });
+    const url = options;
+    return API.get(url)
+      .then(res => res.data)
+      .catch(err => {
+        throw new Error(err?.response?.data?.message ?? "Server error");
+      });
   } else {
     const url = options.url;
     const params = options.params;
-    return API.get(url, { params }).then(res => res.data).catch(err => { throw new Error(err?.response?.data?.message ?? 'Server error') });
+    return API.get(url, { params })
+      .then(res => res.data)
+      .catch(err => {
+        throw new Error(err?.response?.data?.message ?? "Server error");
+      });
   }
 }
