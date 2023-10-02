3a14eb41e0dac9409aaedb7a9679449f745f6d70
TJ Klimoski <tjklimoski@gmail.com>
2023-10-01 22:49:57 -0400

    Use $facet and $project stages to reshape data returned by mongodb

--- app/api/watchparties/search/route.ts
+++ app/api/watchparties/search/route.ts
@@ -1,75 +1,97 @@
 import { milesToMeters } from "@/lib/Geocode";
 import convertToWatchParty from "@/lib/convertWatchPartyData";
 import prisma from "@/prisma/client";
+import { Prisma } from "@prisma/client";
 import { NextRequest, NextResponse as res } from "next/server";
+
+interface SearchData {
+  page: number;
+  results: Prisma.JsonObject[];
+  total_pages: number;
+  total_results: number;
+}
 
 // GET all WatchParties that meet the specified searchParams passed (including a query)
 // Must pass a coordinates array [lon, lat] and mile radius
 export async function GET(req: NextRequest) {
   try {
-    const searchParams = req.nextUrl.searchParams
+    const searchParams = req.nextUrl.searchParams;
 
     // Check for required params for /search API route
-    if (!searchParams.has('coordinates[]')) throw new Error('no coordinates passed')
-    if (!searchParams.has('radius')) throw new Error('No radius passed')
-    if (!searchParams.has('query')) throw new Error('No query passed')
+    if (!searchParams.has("coordinates[]"))
+      throw new Error("no coordinates passed");
+    if (!searchParams.has("radius")) throw new Error("No radius passed");
+    if (!searchParams.has("query")) throw new Error("No query passed");
 
     // extract values from searchParams
-    const searchParamCoords: string[] = searchParams.getAll('coordinates[]')
-    const radius: number = parseInt(searchParams.get('radius') as string)
-    const query: string = searchParams.get('query') as string;
-    const page: number = parseInt(searchParams.get('page') ?? '1')
+    const searchParamCoords: string[] = searchParams.getAll("coordinates[]");
+    const radius: number = parseInt(searchParams.get("radius") as string);
+    const query: string = searchParams.get("query") as string;
+    const page: number = parseInt(searchParams.get("page") ?? "1");
 
     // validate values from searchParams (as needed)
     const coordinates = searchParamCoords.map((coord, index) => {
       const coordinate = parseFloat(coord);
-      if (isNaN(coordinate)) throw new Error('Invalid coordinate')
-      if (index === 0 && (coordinate < -180 || coordinate > 180)) throw new Error('Invalid longitude')
-      if (index === 1 && (coordinate < -90 || coordinate > 90)) throw new Error('Invalid latitude')
+      if (isNaN(coordinate)) throw new Error("Invalid coordinate");
+      if (index === 0 && (coordinate < -180 || coordinate > 180))
+        throw new Error("Invalid longitude");
+      if (index === 1 && (coordinate < -90 || coordinate > 90))
+        throw new Error("Invalid latitude");
       return coordinate;
-    })
-    if (coordinates.length !== 2) throw new Error('Invalid coordinates')
-    if (isNaN(radius) || radius < 1) throw new Error('Invalid radius')
-    if (isNaN(page) || page < 1) throw new Error("Invalid page number")
+    });
+    if (coordinates.length !== 2) throw new Error("Invalid coordinates");
+    if (isNaN(radius) || radius < 1) throw new Error("Invalid radius");
+    if (isNaN(page) || page < 1) throw new Error("Invalid page number");
 
     // convert radius from miles to meters (for mongodb)
-    const radiusMeters = milesToMeters(radius)
+    const radiusMeters = milesToMeters(radius);
 
     const documentsPerPage = 20;
+    const skip = documentsPerPage * (page - 1);
 
-    const watchParties = await prisma.watchParty.aggregateRaw({
-      pipeline: [
-        {
-          $geoNear: {
-            near: { type: "Point", coordinates },
-            distanceField: "dist.calculated", // This is required
-            maxDistance: radiusMeters,
-          }
-        },
-        {
-          $match: {
-            // Only return upcoming events
-            $expr: {
-              $gte: [
-                "$date",
-                "$$NOW"
-              ],
+    const watchParties = await prisma.watchParty
+      .aggregateRaw({
+        pipeline: [
+          // Create $Search stage - which will handle query search in the mediaTitle, near for user radius/coordinates, and date to pull events that are upcoming.
+          {
+            $facet: {
+              results: [{ $skip: skip }, { $limit: documentsPerPage }],
+              metadata: [{ $count: "total" }],
             },
-          }
-        },
-        {
-          // Only return at most 20 documents
-          $limit: documentsPerPage,
-        }
-      ]
-    }).then(res => convertToWatchParty(res))
+          },
+          {
+            $project: {
+              results: 1,
+              total_results: { $arrayElemAt: ["$metadata.total", 0] },
+              page: { $toInt: page },
+              total_pages: {
+                $ceil: {
+                  $divide: [
+                    { $arrayElemAt: ["$metadata.total", 0] },
+                    documentsPerPage,
+                  ],
+                },
+              },
+            },
+          },
+        ],
+      })
+      .then(res => {
+        // extract response from array, clean up results, return data.
+        const data = res[0] as unknown as SearchData;
+        if (!data) throw new Error("Database search failed");
+        return {
+          ...data,
+          results: convertToWatchParty(data.results),
+        };
+      });
 
-    if (!watchParties) throw new Error('Invalid request')
+    if (!watchParties) throw new Error("Invalid request");
 
     // watchParties can be an empty array - meaning nothing found in radius
-    return res.json(watchParties)
+    return res.json(watchParties);
   } catch (err: Error | any) {
-    console.error(err?.message ?? err)
-    return new res(err?.message ?? "request failed", { status: 400 })
+    console.error(err?.message ?? err);
+    return new res(err?.message ?? "request failed", { status: 400 });
   }
 }
