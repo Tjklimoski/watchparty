9e8b46df3d7d7f0b73faed6fc41715bf4fa84726
TJ Klimoski <tjklimoski@gmail.com>
2023-10-07 17:07:59 -0400

    Add client routing to ProfileMenu buttons

--- components/nav/ProfileMenu.tsx
+++ components/nav/ProfileMenu.tsx
@@ -6,32 +6,39 @@ import { BsFillGearFill } from "react-icons/bs";
 import { RiMovieFill } from "react-icons/ri";
 import { FaUser } from "react-icons/fa6";
 import { useRouter } from "next/navigation";
+import useUser from "@/hooks/useUser";
 
 export default function ProfileMenu() {
   const router = useRouter();
+  const { user } = useUser();
 
   return (
     <ul className={`w-full menu menu-md whitespace-nowrap`}>
       <li>
-        <button>
+        <button
+          onClick={() => {
+            if (!user) return;
+            router.push(`/user/${user.id}`);
+          }}
+        >
           <FaUser size={18} />
           My Profile
         </button>
       </li>
       <li>
-        <button>
+        <button onClick={() => router.push("/user/myparties")}>
           <BiSolidParty size={18} />
           My Parties
         </button>
       </li>
       <li>
-        <button>
+        <button onClick={() => router.push("/user/mylist")}>
           <RiMovieFill size={18} />
           My List
         </button>
       </li>
       <li>
-        <button>
+        <button onClick={() => router.push("/user/settings")}>
           <BsFillGearFill size={18} />
           Settings
         </button>
