e34062940619e4bc87391a1c4137b90c1050aec4
TJ Klimoski <tjklimoski@gmail.com>
2023-10-08 20:14:52 -0400

    Update coordinates type for UserLocation from Int to Decimal

--- app/api/user/location/route.ts
+++ app/api/user/location/route.ts
@@ -1,13 +1,29 @@
 import auth from "@/lib/authenticate";
+import prisma from "@/prisma/client";
 import { NextRequest, NextResponse as res } from "next/server";
 
 export async function POST(req: NextRequest) {
   try {
     const user = await auth();
+    if (!user) throw new Error("No user");
+
     const data = await req.json();
 
-    // temp
-    return res.json(user);
+    if (!data.coordinates || !data.city || data.coordinates.length !== 2)
+      throw new Error("Invalid data sent");
+
+    const updatedUser = await prisma.user.update({
+      where: {
+        id: user.id,
+      },
+      data: {
+        location: data,
+      },
+    });
+
+    if (!updatedUser) throw new Error("Failed to set user's location data");
+
+    return res.json(updatedUser);
   } catch (err: Error | any) {
     return new res(err?.message ?? err, { status: 400 });
   }
