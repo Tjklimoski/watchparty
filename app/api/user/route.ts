558abd8a39dc96979f9072d0a3cd49c66c3a91dc
TJ Klimoski <tjklimoski@gmail.com>
2023-10-12 15:01:05 -0400

    Add DELETE request to /user route with try catch block

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -4,7 +4,20 @@ import { NextRequest, NextResponse as res } from "next/server";
 export async function GET(req: NextRequest) {
   const user = await auth();
 
-  if (!user) return new res('No user logged in', { status: 400 })
+  if (!user) return new res("No user logged in", { status: 400 });
 
-  return res.json(user)
+  return res.json(user);
+}
+
+export async function DELETE(req: NextRequest) {
+  try {
+    const user = await auth();
+
+    if (!user) throw new Error("No user logged in");
+  } catch (err: Error | any) {
+    return new res(
+      err?.message ?? err?.response?.data ?? err ?? "Request failed",
+      { status: 400 }
+    );
+  }
 }
