dca627cfa45b36fee5ae49c8f34f9cc7f6ff7cbb
TJ Klimoski <tjklimoski@gmail.com>
2023-10-12 15:06:28 -0400

    Make delete request to database to delete the currently logged in user

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,4 +1,5 @@
 import auth from "@/lib/authenticate";
+import prisma from "@/prisma/client";
 import { NextRequest, NextResponse as res } from "next/server";
 
 export async function GET(req: NextRequest) {
@@ -14,6 +15,16 @@ export async function DELETE(req: NextRequest) {
     const user = await auth();
 
     if (!user) throw new Error("No user logged in");
+
+    // prisma request to db to delete user
+    // check that cascading delete of watchparty works as intended
+    const deleteUser = await prisma.user.delete({
+      where: {
+        id: user.id,
+      },
+    });
+
+    if (!deleteUser) throw new Error("Failed to delete user");
   } catch (err: Error | any) {
     return new res(
       err?.message ?? err?.response?.data ?? err ?? "Request failed",
