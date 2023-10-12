af8f47d07505b7dca094d338b802c6ee49a260aa
TJ Klimoski <tjklimoski@gmail.com>
2023-10-12 16:35:51 -0400

    Parse page searchParam value

--- app/api/user/my-watchparties/route.ts
+++ app/api/user/my-watchparties/route.ts
@@ -5,21 +5,49 @@ import { NextRequest, NextResponse as res } from "next/server";
 export async function GET(req: NextRequest) {
   try {
     const user = await auth();
+    if (!user) throw new Error("No current user");
 
-    if (!user) throw new Error('No current user')
+    // parse Page searchParam value
+    let page: string | number | null = req.nextUrl.searchParams.get("page");
+    if (!page) throw new Error("No page searchParam passed");
+    page = parseInt(page);
+    if (isNaN(page)) throw new Error("Page serachParam must be a number");
 
-    const userWithParties = await prisma.user.findUniqueOrThrow({
+    // set pagination variables
+    const take = 20;
+    const skip = (page - 1) * take;
+
+    const myWatchParties = prisma.watchParty.findMany({
       where: {
-        id: user.id
+        userId: user.id,
       },
-      include: {
-        myWatchParties: true
-      }
-    })
+    });
 
-    return res.json(userWithParties)
+    return res.json(userWithParties);
   } catch (err: Error | any) {
-    console.error(err)
-    return new res(err?.message ?? "Request Failed", { status: 400 })
+    console.error(err);
+    return new res(err?.message ?? "Request Failed", { status: 400 });
   }
 }
+
+// export async function GET(req: NextRequest) {
+//   try {
+//     const user = await auth();
+
+//     if (!user) throw new Error("No current user");
+
+//     const userWithParties = await prisma.user.findUniqueOrThrow({
+//       where: {
+//         id: user.id,
+//       },
+//       include: {
+//         myWatchParties: true,
+//       },
+//     });
+
+//     return res.json(userWithParties);
+//   } catch (err: Error | any) {
+//     console.error(err);
+//     return new res(err?.message ?? "Request Failed", { status: 400 });
+//   }
+// }
