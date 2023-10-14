c73dcfb4134ce6cef172eadb169f4c51e0ee57be
TJ Klimoski <tjklimoski@gmail.com>
2023-10-14 16:59:51 -0400

    Authenticate current user if an updatedPassword was passed

--- app/api/user/settings/route.ts
+++ app/api/user/settings/route.ts
@@ -1,4 +1,6 @@
+import auth from "@/lib/authenticate";
 import { NextRequest, NextResponse as res } from "next/server";
+import bcrypt from "bcrypt";
 
 interface ReqBody {
   city?: string;
@@ -9,6 +11,28 @@ interface ReqBody {
 }
 
 export async function PATCH(req: NextRequest) {
-  const { city, radius, name, updatedPassword, currentPassword }: ReqBody =
-    await req.json();
+  try {
+    const { city, radius, name, updatedPassword, currentPassword }: ReqBody =
+      await req.json();
+
+    const user = await auth();
+    if (!user) throw new Error("No current user");
+
+    // If user passed a new password, authenticate user.
+    if (updatedPassword) {
+      // check if the sent a current password
+      if (!currentPassword)
+        throw new Error(
+          "No current password passed, cannot update your password"
+        );
+      // Check that they have a password to update
+      if (!user.password)
+        throw new Error("Logged in using OAuth, no password to update");
+      // throw error if currentPasswords do not match
+      if (!bcrypt.compare(currentPassword, user.password))
+        throw new Error("Unable to validate current user");
+    }
+  } catch (err: Error | any) {
+    return new res(err?.message ?? err ?? "Update failed", { status: 400 });
+  }
 }
