3ca6dc9cc69dd6e5a2673ce3a8e7b036a075b91f
TJ Klimoski <tjklimoski@gmail.com>
2023-10-16 18:43:44 -0400

    Extract needed info from returned user document

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -1,27 +1,54 @@
 import { NextRequest, NextResponse as res } from "next/server";
 import prisma from "@/prisma/client";
-import { LimitedUser } from "@/types";
+import { LimitedUser, User } from "@/types";
 
 export async function GET(
   req: NextRequest,
   { params }: { params: { id: string } }
 ) {
-  const { id } = params;
+  const { id: userId } = params;
 
   try {
-    const user: LimitedUser = await prisma.user.findUnique({
+    const user = await prisma.user.findUnique({
       where: {
-        id,
+        id: userId,
       },
       include: {
-        myWatchParties: true,
-        goingToWatchParties: true,
+        myWatchParties: {
+          orderBy: {
+            date: "asc",
+          },
+        },
+        // Only fetch the watchparties the user has already attended. Not ones they plan to attend in the future
+        goingToWatchParties: {
+          where: {
+            date: {
+              lte: new Date(),
+            },
+          },
+          orderBy: {
+            date: "desc",
+          },
+        },
       },
     });
 
     if (!user) throw new Error("Invalid id");
 
     // Need to extract: id, name, image, location.city, hosted COUNT, hosting COUNT, attended COUNT, createdAT, myList
+
+    const {
+      id,
+      name,
+      image,
+      createdAt,
+      myList,
+      myWatchParties,
+      goingToWatchParties,
+    } = user;
+    const city = user.location?.city;
+
+    const limitedUser = {};
 
     return res.json(user);
   } catch (err) {
