e97a3b839be8e67a84226b19ed558f7307fd0afd
TJ Klimoski <tjklimoski@gmail.com>
2023-10-16 18:08:13 -0400

    Fetch specific user's data when request is made to /users/[id]

--- app/api/users/[id]/route.ts
+++ app/api/users/[id]/route.ts
@@ -2,26 +2,29 @@ import { NextRequest, NextResponse as res } from "next/server";
 import prisma from "@/prisma/client";
 import { LimitedUser } from "@/types";
 
-export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
   const { id } = params;
 
   try {
     const user: LimitedUser = await prisma.user.findUnique({
       where: {
-        id
+        id,
       },
-      select: {
-        id: true,
-        name: true,
-        image: true,
-      }
+      include: {
+        myWatchParties: true,
+        goingToWatchParties: true,
+      },
     });
 
-    if (!user) throw new Error('Invalid id')
+    if (!user) throw new Error("Invalid id");
+
+    // Need to extract: id, name, image, location.city, hosted COUNT, hosting COUNT, attended COUNT, createdAT, myList
 
     return res.json(user);
-
   } catch (err) {
-    return new res("Invalid id", { status: 400 })
+    return new res("Invalid id", { status: 400 });
   }
 }
