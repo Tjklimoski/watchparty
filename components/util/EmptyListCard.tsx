2d552d04c8b9d0073b0f02e61677454d7522ea13
TJ Klimoski <tjklimoski@gmail.com>
2023-10-17 19:13:48 -0400

    Create EmptyListCard component based off NoWatchPartiesCard component

---
+++ components/util/EmptyListCard.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+
+interface EmptyListCardProps {
+  message?: string;
+}
+
+export default function EmptyListCard({
+  message = "Empty",
+}: EmptyListCardProps) {
+  return (
+    <div className="relative w-48 @lg:w-52 @3xl:w-64 @5xl:w-72 aspect-video rounded-sm drop-shadow-lg overflow-hidden">
+      <Image
+        className="object-cover brightness-90 rounded-sm"
+        src="/img/placeholder-md.jpg"
+        alt="No Content"
+        fill
+        sizes="500px"
+      />
+
+      <div className="absolute left-0 bottom-0 right-0 h-1/2 bg-gradient-to-t from-black via-black via-45% to-transparent p-1 @lg:p-2 flex items-end select-none">
+        <h3 className="font-semibold text-md @lg:text-lg @3xl:text-xl break-balance webkit-truncate">
+          {message}
+        </h3>
+      </div>
+    </div>
+  );
+}
