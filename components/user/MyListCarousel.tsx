cab7a053272835af893db320d303a7bbf7df9b0b
TJ Klimoski <tjklimoski@gmail.com>
2023-10-17 19:11:02 -0400

    Create loading state for MyListCarousel component

--- components/user/MyListCarousel.tsx
+++ components/user/MyListCarousel.tsx
@@ -8,16 +8,30 @@ import { Movie, MyListItem, TVShow } from "@/types";
 import Skeleton from "../util/Skeleton";
 import MediaCard from "../media/MediaCard";
 
-interface MediaCarouselProps {
-  list: MyListItem[];
+interface MyListCarouselProps {
+  list: MyListItem[] | undefined;
 }
 
 interface FetchMediaContainerProps {
   myListItem: MyListItem;
 }
 
-export default function MediaCarousel({ list }: MediaCarouselProps) {
-  return (
+export default function MyListCarousel({ list }: MyListCarouselProps) {
+  const MediaCardSkeletons = Array(6)
+    .fill(null)
+    .map((item, i) => (
+      <Skeleton
+        key={i}
+        className="h-full w-48 @lg:w-52 @3xl:w-64 @5xl:w-72 aspect-video rounded-sm"
+      />
+    ));
+
+  return !list ? (
+    <>
+      <Skeleton className="h-8 w-1/6 min-w-[180px]" />
+      <Carousel tight>{MediaCardSkeletons}</Carousel>
+    </>
+  ) : (
     <>
       <h3 className="text-xl sm:text-2xl mb-2 font-bold">
         Recently Added to My List
