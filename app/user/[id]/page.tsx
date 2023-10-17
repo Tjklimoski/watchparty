fd0dd8cbb45efed3bfb64cafd8b2409b9372fd6f
TJ Klimoski <tjklimoski@gmail.com>
2023-10-17 14:00:11 -0400

    Create user profile page

--- app/user/[id]/page.tsx
+++ app/user/[id]/page.tsx
@@ -1,1 +1,12 @@
 // This is the publicly available page that's visible to any user. Will show what this user's myList is, their watchParties, and badges for their achievments (Create their first watchparty, attend their first watchparty, host 5 watchparties, attend 10 watchparties, add 10 movies to their list)
+
+import Container from "@/components/util/Container";
+import React from "react";
+
+export default function UserProfilePage() {
+  return (
+    <main className="min-h-screen">
+      <Container>User Profile Page</Container>
+    </main>
+  );
+}
