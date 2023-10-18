c9007ad4ae096025440df46af38f20b8976d236c
TJ Klimoski <tjklimoski@gmail.com>
2023-10-17 22:44:50 -0400

    Modify inline padding on Container component to make padding smaller at smaller screen sizes

--- components/util/Container.tsx
+++ components/util/Container.tsx
@@ -4,7 +4,7 @@ interface PageContainerProps {
 
 export default function Container({ children }: PageContainerProps) {
   return (
-    <div className="px-6 py-2 md:px-12 md:py-4 w-full h-full max-w-[1440px] mx-auto">
+    <div className="px-2 py-2 xs:px-4 sm:px-6 md:px-12 md:py-4 w-full h-full max-w-[1440px] mx-auto">
       {children}
     </div>
   );
