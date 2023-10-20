5a683580eba1bef02397c5e70f02a7abf7befb97
TJ Klimoski <tjklimoski@gmail.com>
2023-10-19 22:09:03 -0400

    Get client serachParams on UserMyListPage and fetch my list data

--- app/user/mylist/page.tsx
+++ app/user/mylist/page.tsx
@@ -1,9 +1,25 @@
 "use client";
 
+import BackBtn from "@/components/util/BackBtn";
 import Container from "@/components/util/Container";
+import PageCount from "@/components/util/PageCount";
+import APIFetcher from "@/lib/APIFetcher";
 import React from "react";
+import useSWR from "swr";
 
-export default function UserMyListPage() {
+export default function UserMyListPage({
+  searchParams,
+}: {
+  searchParams: { page?: string };
+}) {
+  const { page = "1" } = searchParams;
+  const { data, isLoading, error } = useSWR(
+    `/user/my-list?page=${page}`,
+    APIFetcher
+  );
+
+  if (!isLoading && error) throw new Error(error);
+
   return (
     <main className="min-h-screen">
       <Container>
@@ -13,7 +29,12 @@ export default function UserMyListPage() {
           </h2>
         </header>
 
-        <section>{/* results */}</section>
+        <section>
+          <div className="text-lg flex justify-between items-center mb-4">
+            <BackBtn />
+            <PageCount totalPages={data?.total_pages} />
+          </div>
+        </section>
       </Container>
     </main>
   );
