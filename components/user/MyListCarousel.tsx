1e1d4d175e9f4377d5636c44f6bd2a4934129b32
TJ Klimoski <tjklimoski@gmail.com>
2023-10-19 22:27:38 -0400

    Implement MyListItemCard component in MyLKistCarousel

--- components/user/MyListCarousel.tsx
+++ components/user/MyListCarousel.tsx
@@ -2,19 +2,13 @@
 
 import React from "react";
 import Carousel from "../util/Carousel";
-import fetcher from "@/lib/TMDBFetcher";
-import useSWRImmutable from "swr/immutable";
-import { Movie, MyListItem, TVShow } from "@/types";
+import { MyListItem } from "@/types";
 import Skeleton from "../util/Skeleton";
-import MediaCard from "../media/MediaCard";
 import EmptyListCard from "../util/EmptyListCard";
+import MyListItemCard from "./MyListItemCard";
 
 interface MyListCarouselProps {
   list: MyListItem[] | undefined;
-}
-
-interface FetchMediaContainerProps {
-  myListItem: MyListItem;
 }
 
 export default function MyListCarousel({ list }: MyListCarouselProps) {
@@ -40,25 +34,9 @@ export default function MyListCarousel({ list }: MyListCarouselProps) {
       <Carousel tight>
         {list.length === 0 && <EmptyListCard message="No Movies or TV Shows" />}
         {list.map(listItem => (
-          <FetchMediaContainer key={listItem.id} myListItem={listItem} />
+          <MyListItemCard key={listItem.id} myListItem={listItem} />
         ))}
       </Carousel>
     </>
-  );
-}
-
-// Placed in seperate component in order to utalise useSWR
-function FetchMediaContainer({ myListItem }: FetchMediaContainerProps) {
-  const { data: media, isLoading } = useSWRImmutable<Movie | TVShow>(
-    `/${myListItem.media_type}/${myListItem.id}`,
-    fetcher
-  );
-
-  if (!media) return null;
-
-  return isLoading ? (
-    <Skeleton className="h-full w-48 @lg:w-52 @3xl:w-64 @5xl:w-72 aspect-video rounded-sm" />
-  ) : (
-    <MediaCard media={media} />
   );
 }
