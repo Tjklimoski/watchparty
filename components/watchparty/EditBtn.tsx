24e50f23e2618380295afff3d3112f196b89141f
TJ Klimoski <tjklimoski@gmail.com>
2023-10-23 18:19:51 -0400

    Add resize prop to EditBtn component to denote if the btn can adjust it's size via container queries

--- components/watchparty/EditBtn.tsx
+++ components/watchparty/EditBtn.tsx
@@ -5,20 +5,25 @@ import { MdEdit } from "react-icons/md";
 
 interface EditBtnProps {
   watchPartyId: string;
+  resize?: boolean;
 }
 
-export default function EditBtn({ watchPartyId }: EditBtnProps) {
+export default function EditBtn({ watchPartyId, resize }: EditBtnProps) {
   const router = useRouter();
 
   // btn and icon text sizes using container querys for WatchPartySearchResult component
   return (
     <button
-      className="btn-sm @xs:btn-md btn btn-circle btn-primary btn-outline border-2 grid place-items-center tooltip tooltip-info normal-case"
+      className={`${
+        resize ? "btn-sm @xs:btn-md" : "btn-md"
+      } btn btn-circle btn-primary btn-outline border-2 grid place-items-center tooltip tooltip-info normal-case`}
       data-tip="Edit"
       aria-label="Edit your WatchParty"
       onClick={() => router.push(`/watchparty/${watchPartyId}/edit`)}
     >
-      <MdEdit className="text-[20px] @xs:text-[30px]" />
+      <MdEdit
+        className={resize ? "text-[20px] @xs:text-[30px]" : "text-[30px]"}
+      />
     </button>
   );
 }
