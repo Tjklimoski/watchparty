c80225f9af16da21f3662c4ed4f723982c544b75
TJ Klimoski <tjklimoski@gmail.com>
2023-10-22 20:36:00 -0400

    Map over filter btns array to create button elements

--- app/user/myparties/page.tsx
+++ app/user/myparties/page.tsx
@@ -97,67 +97,19 @@ export default function UserMyPartiesPage({
 
         <section>
           <div className="flex flex-wrap gap-2 mb-4 sm:mb-8">
-            {/* Buttons to filter results */}
-            <button
-              className="btn-sm xs:btn-md btn btn-primary btn-outline border-2 rounded-full"
-              value=""
-              aria-current={true}
-              onClick={e => {
-                setRoute(e.currentTarget.value);
-              }}
-            >
-              All
-            </button>
-            <button
-              className="btn-sm xs:btn-md btn btn-primary btn-outline border-2 rounded-full"
-              value="/today"
-              aria-current={false}
-              onClick={e => {
-                setRoute(e.currentTarget.value);
-              }}
-            >
-              Today
-            </button>
-            <button
-              className="btn-sm xs:btn-md btn btn-primary btn-outline border-2 rounded-full"
-              value="/hosting"
-              aria-current={false}
-              onClick={e => {
-                setRoute(e.currentTarget.value);
-              }}
-            >
-              Hosting
-            </button>
-            <button
-              className="btn-sm xs:btn-md btn btn-primary btn-outline border-2 rounded-full"
-              value="/upcoming"
-              aria-current={false}
-              onClick={e => {
-                setRoute(e.currentTarget.value);
-              }}
-            >
-              Upcoming
-            </button>
-            <button
-              className="btn-sm xs:btn-md btn btn-primary btn-outline border-2 rounded-full"
-              value="/following"
-              aria-current={false}
-              onClick={e => {
-                setRoute(e.currentTarget.value);
-              }}
-            >
-              Following
-            </button>
-            <button
-              className="btn-sm xs:btn-md btn btn-primary btn-outline border-2 rounded-full"
-              value="/attended"
-              aria-current={false}
-              onClick={e => {
-                setRoute(e.currentTarget.value);
-              }}
-            >
-              Attended
-            </button>
+            {FILTER_BTNS.map(btn => (
+              <button
+                key={btn.value}
+                className="btn-sm xs:btn-md btn btn-primary btn-outline border-2 rounded-full"
+                value={btn.value}
+                aria-current={true}
+                onClick={e => {
+                  setRoute(e.currentTarget.value);
+                }}
+              >
+                {btn.name}
+              </button>
+            ))}
           </div>
 
           <div className="text-lg flex justify-between items-center mb-4">
