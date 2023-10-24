529cdb5ef0400d5d2909400f0d6ecbccb8b6d154
TJ Klimoski <tjklimoski@gmail.com>
2023-10-24 13:28:34 -0400

    Setup settings page

--- app/user/settings/page.tsx
+++ app/user/settings/page.tsx
@@ -1,1 +1,6 @@
 // abilty to change name, email (if not OAuth, check for associated accounts through prisma), password (if not OAuth), image, location, & radius
+import React from "react";
+
+export default function SettingsPage() {
+  return <div>page</div>;
+}
