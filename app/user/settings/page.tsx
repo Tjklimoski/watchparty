9b0fea27b44d162accbabd4190dd9a8dcf820765
TJ Klimoski <tjklimoski@gmail.com>
2023-10-25 14:05:17 -0400

    Layout Settings page

--- app/user/settings/page.tsx
+++ app/user/settings/page.tsx
@@ -1,6 +1,22 @@
 // abilty to change name, email (if not OAuth, check for associated accounts through prisma), password (if not OAuth), image, location, & radius
+import UserPageHeading from "@/components/user/UserPageHeading";
+import Container from "@/components/util/Container";
 import React from "react";
 
 export default function SettingsPage() {
-  return <div>page</div>;
+  return (
+    <main className="min-h-screen">
+      <Container>
+        <UserPageHeading title="Settings" />
+
+        <section>
+          <form>
+            {/* Settings */}
+            {/* submit button */}
+          </form>
+          {/* Delete account button */}
+        </section>
+      </Container>
+    </main>
+  );
 }
