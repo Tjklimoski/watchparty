0dd7e34234c37bb033331648ba770b35a8866964
TJ Klimoski <tjklimoski@gmail.com>
2023-10-30 13:01:00 -0400

    set the outline offset to 0 on the focus state of the Select component

--- components/form/Select.tsx
+++ components/form/Select.tsx
@@ -7,7 +7,7 @@ interface SelectProps extends React.SelectHTMLAttributes<HTMLSelectElement> {
 export default function Select({ children, className, ...props }: SelectProps) {
   return (
     <select
-      className={`select text-sm sm:text-base h-10 min-h-[2.5rem] sm:h-11 sm:min-h-[2.75rem] bg-neutral focus:outline-primary ${className}`}
+      className={`select text-sm sm:text-base h-10 min-h-[2.5rem] sm:h-11 sm:min-h-[2.75rem] bg-neutral focus:outline-primary focus:outline-offset-0 ${className}`}
       {...props}
     >
       {children}
