9fe7a4eadd72a2e7750e9c329168adc62b93286f
TJ Klimoski <tjklimoski@gmail.com>
2023-11-02 13:31:06 -0400

    Clean up code in /api/auth routes

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,40 +1,41 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/client";
-import bcrypt from 'bcrypt'
+import bcrypt from "bcrypt";
 import { capitalize } from "@/lib/stringModifications";
 import { RegisterData } from "@/types";
 
 export async function POST(req: NextRequest) {
   try {
-    const { email, password }: RegisterData = await req.json()
+    const { email, password }: RegisterData = await req.json();
 
     // Check to see if user already exists:
     const user = await prisma.user.findUnique({
       where: {
-        email
-      }
+        email,
+      },
     });
 
-    if (user) throw new Error('User already exists');
+    if (user) throw new Error("User already exists");
 
     const hashedPassword = await bcrypt.hash(password, 12);
 
-    // Create a name for the user based of the user's email:
-    const name = capitalize(email.split('@')[0]);
+    // Create a name for the user based off the user's email:
+    const name = capitalize(email.split("@")[0]);
 
     const newUser = await prisma.user.create({
       data: {
         email,
         password: hashedPassword,
         name,
-        image: '',
+        image: "",
         emailVerified: new Date(),
-      }
-    })
+      },
+    });
 
     return NextResponse.json(newUser);
-
   } catch (error: Error | any) {
-    return NextResponse.json(error.message || 'Error, try again', { status: 400 })
+    return NextResponse.json(error.message || "Error, try again", {
+      status: 400,
+    });
   }
 }
